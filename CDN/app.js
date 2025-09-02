2b0f16b8695a196ab596b63e4fb82033658a236d
satyamra1 <raisatyam710@gmail.com>
2025-09-02 12:31:29 +0530

    Vue Notes

---
+++ CDN/app.js
@@ -0,0 +1,7 @@
+// alert("hello");
+
+const app =Vue.createApp({
+    template:"<h1>VUE JS TUTORIAL 2</h1>"
+}) 
+
+app.mount("#app");
---
+++ CLI/vue-project/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Email: {{ email }}</h1>
+    <h1>Email: {{ mobile }}</h1>
+    <h1>Email: {{ mobile=8888 }}</h1>
+
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            email: "satyam@gmail.com",
+            mobile: 9999 
+        }
+    }
+}
+
+</script>
+
+<style>
+h1{
+    color: red;
+}
+
+</style>
